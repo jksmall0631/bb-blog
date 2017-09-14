6f67fc2859ab24e6f19321c28e3b66de6523562c
Joshua Small <jksmall0631@gmail.com>
2017-09-13 22:38:57 -0600

    fix bugs with collapsible header

--- public/components/Nav/Nav.js
+++ public/components/Nav/Nav.js
@@ -9,7 +9,7 @@ export default class Nav extends Component {
     this.state = {
       showNavBtn: false,
       collapsed: true
-    }
+    };
     this.collapseNav = this.collapseNav.bind(this);
   }
 
@@ -25,26 +25,35 @@ export default class Nav extends Component {
     }
   }
 
-  showNav() {
-    let navBar = document.querySelector(".nav-bar")
+  toggleNav() {
+    let navBar = document.querySelector(".nav-bar");
     let nav = document.querySelector(".nav-cont");
-    if(nav.style.bottom === "0px") {
+    if (nav.style.bottom === "0px") {
       nav.style.bottom = "150px";
       navBar.style.height = "25px";
-      this.setState({ collapsed: true})
-    }
-    else {
+      this.setState({ collapsed: true });
+    } else {
       nav.style.bottom = "0px";
       navBar.style.height = "120px";
-      this.setState({ collapsed: false})
+      this.setState({ collapsed: false });
     }
   }
 
   componentDidMount() {
+    this.checkWidth();
+    window.addEventListener("resize", () => {
+      this.checkWidth();
+    });
+  }
+
+  checkWidth() {
     if (window.innerWidth < 700) {
-      this.setState({ showNavBtn: true })
-    }
-    else {
+      this.setState({ showNavBtn: true });
+    } else {
+      this.setState({ showNavBtn: false });
+      this.setState({ collapsed: true });
+      document.querySelector(".nav-bar").style.height = "25px";
+      document.querySelector(".nav-cont").style.bottom = "150px";
       window.addEventListener("scroll", this.collapseNav);
     }
   }
@@ -57,54 +66,54 @@ export default class Nav extends Component {
     let buttonImg = null;
     buttonImg = this.state.collapsed
       ? <img src="../../resources/menu.png" />
-      : <img src="../../resources/cancel.png" />
+      : <img src="../../resources/cancel.png" />;
 
     return (
       <nav className="nav-bar">
-      { this.state.showNavBtn
-        ? <button className="menu-btn" onClick={() => this.showNav()}>
-          {buttonImg}
-        </button>
-        : null }
-      <div className="nav-cont" style={{bottom: "150px"}}>
-        <ul>
-          {/* <li>
+        {this.state.showNavBtn
+          ? <button className="menu-btn" onClick={() => this.toggleNav()}>
+              {buttonImg}
+            </button>
+          : null}
+        <div className="nav-cont" style={{ bottom: "150px" }}>
+          <ul>
+            {/* <li>
             <a className="nav" href="#welcome">
               . &nbsp;&nbsp; WELCOME &nbsp;&nbsp; .
             </a>
           </li> */}
-          <li>
-            <a className="nav" href="#books">
-              . &nbsp;&nbsp; BOOKS &nbsp;&nbsp; .
-            </a>
-          </li>
-          <li>
-            <a className="nav" href="#events">
-              . &nbsp;&nbsp; CLASSES &nbsp;&nbsp; .
-            </a>
-          </li>
-          {/* <li>
+            <li>
+              <a className="nav" href="#books">
+                . &nbsp;&nbsp; BOOKS &nbsp;&nbsp; .
+              </a>
+            </li>
+            <li>
+              <a className="nav" href="#events">
+                . &nbsp;&nbsp; CLASSES &nbsp;&nbsp; .
+              </a>
+            </li>
+            {/* <li>
             <a className="nav" href="#blog">
               . &nbsp;&nbsp; BLOG &nbsp;&nbsp; .
             </a>
           </li> */}
-          <li>
-            <a className="nav" href="#about">
-              . &nbsp;&nbsp; ABOUT &nbsp;&nbsp; .
-            </a>
-          </li>
-          <li>
-            <a className="nav" href="#subscribe">
-              . &nbsp;&nbsp; SUBSCRIBE &nbsp;&nbsp; .
-            </a>
-          </li>
-          <li>
-            <a className="nav" href="#contact">
-              . &nbsp;&nbsp; CONTACT &nbsp;&nbsp; .
-            </a>
-          </li>
-        </ul>
-      </div>
+            <li>
+              <a className="nav" href="#about">
+                . &nbsp;&nbsp; ABOUT &nbsp;&nbsp; .
+              </a>
+            </li>
+            <li>
+              <a className="nav" href="#subscribe">
+                . &nbsp;&nbsp; SUBSCRIBE &nbsp;&nbsp; .
+              </a>
+            </li>
+            <li>
+              <a className="nav" href="#contact">
+                . &nbsp;&nbsp; CONTACT &nbsp;&nbsp; .
+              </a>
+            </li>
+          </ul>
+        </div>
       </nav>
     );
   }

0ceacab75f4445a1aed165c676237f2d1375bd46
Joshua Small <jksmall0631@gmail.com>
2017-11-07 16:11:48 -0700

    add route that responds with bundle.js.gz instead of bundle.js

--- server.js
+++ server.js
@@ -20,6 +20,14 @@ app.use(function(req, res, next) {
   next();
 });
 
+app.get('*/main.bundle.js', function (req, res, next) {
+  console.log("bla");
+  console.log(req.url);
+  req.url = req.url + '.gz';
+  res.set('Content-Encoding', 'gzip');
+  next();
+});
+
 app.use(express.static("public"));
 app.use("/static", express.static(__dirname + "/public"));
 app.use(bodyParser.json());
@@ -99,12 +107,6 @@ app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "./public/index.html"));
 });
 
-app.get('*.js', function (req, res, next) {
-  console.log("bla");
-  req.url = req.url + '.gz';
-  res.set('Content-Encoding', 'gzip');
-  next();
-});
 
 app.listen(app.get("port"), () => {
   console.log(`Running on ${app.get("port")}`);
